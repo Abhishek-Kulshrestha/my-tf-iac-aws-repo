757560acfa7728a790a4947c8cabfd4f436e044b
Abhishek-Kulshrestha <87300489+Abhishek-Kulshrestha@users.noreply.github.com>
2022-08-08 17:41:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-2"
+  default     = "ap-south-1"
 }
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+  default     =  "myAugust2022Key"
 }
 
 variable "instance_type" {
@@ -24,6 +24,6 @@ variable "tag_name" {
 }
 
 variable "ami_id" {
-  description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0b9064170e32bde34"
+  description = "AMI for Ec2 instance"
+  default     = "ami-076e3a557efe1aa9c"
 }
